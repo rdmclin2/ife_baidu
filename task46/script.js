b7cff6fa6906ca3ae161865581f6bdf8c511e5e5
rdmclin2 <rdmclin2@163.com>
2016-05-02 18:07:43 +0800

    rewrite

--- task46/script.js
+++ task46/script.js
@@ -48,16 +48,17 @@ for (let i = 0; i < block_height; i++) {
  * @param y
  */
 let Agent = function (i, j) {
-    this.x = i * block_length + block_length / 2;
-    this.y = j * block_length + block_length / 2;
-    this.speed = 256;
+    this.x = i ;
+    this.y = j ;
     this.radius = (block_length - 4) / 2;
     this.color = '#49b626';
 };
 
 Agent.prototype.draw = function () {
+    var posX = this.x * block_length + block_length / 2;
+    var posY = this.y* block_length + block_length / 2;
     ctx.beginPath();
-    ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
+    ctx.arc(posX,posY, this.radius, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.fillStyle = this.color;
     ctx.fill();
@@ -69,52 +70,54 @@ Agent.prototype.draw = function () {
  * @param y
  */
 let File = function (i, j) {
-    this.x = i * block_length;
-    this.y = j * block_length;
+    this.x = i ;
+    this.y = j ;
     this.color = '#f2ae3b';
 };
 
 File.prototype.draw = function () {
+    var posX = this.x * block_length;
+    var posY = this.y * block_length;
     // 填充三角形
     ctx.beginPath();
-    ctx.moveTo(this.x, this.y);
-    ctx.lineTo(this.x + block_length, this.y);
-    ctx.lineTo(this.x + block_length / 2, this.y + block_length / 2 * 1.7);
+    ctx.moveTo(posX, posY);
+    ctx.lineTo(posX + block_length, posY);
+    ctx.lineTo(posX + block_length / 2, posY + block_length / 2 * Math.sqrt(3));
     ctx.fillStyle = this.color;
     ctx.fill();
 };
 
 let Block = function (i, j) {
-    this.x = i * block_length;
-    this.y = j * block_length;
+    this.x = i ;
+    this.y = j ;
     this.color = '#fee6ce';
 }
 
 Block.prototype.draw = function () {
     ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, block_length, block_length);
+    ctx.fillRect(this.x* block_length, this.y* block_length, block_length, block_length);
 }
 
 let Obstacle = function (i, j) {
-    this.x = i * block_length;
-    this.y = j * block_length;
+    this.x = i ;
+    this.y = j ;
     this.color = '#2e1e1e';
 }
 
 Obstacle.prototype.draw = function () {
     ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, block_length, block_length);
+    ctx.fillRect(this.x* block_length, this.y* block_length, block_length, block_length);
 }
 
 let Wall = function (i, j) {
-    this.x = i * block_length;
-    this.y = j * block_length;
+    this.x = i ;
+    this.y = j ;
     this.color = '#3e4958';
 }
 
 Wall.prototype.draw = function () {
     ctx.fillStyle = this.color;
-    ctx.fillRect(this.x, this.y, block_length, block_length);
+    ctx.fillRect(this.x* block_length, this.y* block_length, block_length, block_length);
 }
 
 /**
@@ -148,83 +151,48 @@ let init_canvas = function () {
         }
     }
 
+    let startX = block_width / 2;
+    let startY= 1;
+
+    let endX = block_width / 2;
+    let endY= block_height - 2;
+
+    let path = findPath(startX,startY,endX,endY);
+    if(path.length === 0 ){
+        init_canvas();
+        return [];
+    }
+
     //特工,和文件
-    agent = new Agent(block_width / 2, 1);
-    file = new File(block_width / 2, block_height - 2);
+    agent = new Agent(startX, startY);
+    file = new File(endX,endY);
 
 }
 
-//let update = function (modifier) {
-//    let width = agent.speed * modifier;
-//    let oldx = agent.x;
-//    let oldy = agent.y;
-//
-//    //墙壁碰撞检测
-//    if (38 in keysDown) { // 用户按的是↑
-//        if (agent.y - width > agent.radius + block_length) {
-//            agent.y -= width;
-//        }
-//    }
-//    if (40 in keysDown) { // 用户按的是↓
-//        if (agent.y + width < canvas.height - agent.radius - block_length) {
-//            agent.y += width;
-//        }
-//    }
-//    if (37 in keysDown) { // 用户按的是←
-//        if (agent.x - width > agent.radius + block_length) {
-//            agent.x -= width;
-//        }
-//    }
-//    if (39 in keysDown) { // 用户按的是→
-//        if (agent.x + width < canvas.width - agent.radius - block_length) {
-//            agent.x += width;
-//        }
-//    }
-//
-//    //alert(obstacles.length);
-//    // 墙壁碰撞检测
-//    for (let i = obstacles.length - 1; i >= 0; i--) {
-//        if ((agent.x - agent.radius) < (obstacles[i].x + block_length) && (agent.x + agent.radius) > obstacles[i].x && (agent.y - agent.radius) < (obstacles[i].y + block_length) && (agent.y + agent.radius) > obstacles[i].y) {
-//            agent.x = oldx;
-//            agent.y = oldy;
-//        }
-//    }
-//
-//    // 是否拿到文件
-//    if ((agent.x - agent.radius) <= (file.x + block_length) && (agent.x + agent.radius) >= file.x && (agent.y - agent.radius) <= (file.y + block_length / 2 * 1.7) && (agent.y + agent.radius) >= file.y) {
-//        init_canvas();
-//    }
-//}
-
-//addEventListener("keydown", function (e) {
-//    keysDown[e.keyCode] = true;
-//}, false);
-//
-//addEventListener("keyup", function (e) {
-//    delete keysDown[e.keyCode];
-//}, false);
-
-
-canvas.addEventListener("click",function(event){
-    let e = event;
-    let startX = Math.floor(agent.x /block_length);
-    let startY= Math.floor(agent.y /block_length);
-
-    let endX = Math.floor((e.clientX - canvas.offsetLeft)/ block_length);
-    let endY= Math.floor((e.clientY - canvas.offsetTop)/block_length);
-
+function findPath(startX,startY,endX,endY){
     let grid = new Grid(block_width,block_height);
+
     for (let i = 0; i < block_height; i++) {
         for (let j = 0; j < block_width; j++) {
             if(boards[i][j] instanceof Wall || boards[i][j] instanceof Obstacle){
-                let x = boards[i][j].x / block_length;
-                let y = boards[i][j].y / block_length;
-                grid.setWalkableAt(x, y, false);
+                grid.setWalkableAt(boards[i][j].x, boards[i][j].y, false);
             }
         }
     }
     let path = finder.findPath(startX, startY, endX, endY, grid);
-    console.log(JSON.stringify(path));
+    return path;
+}
+
+canvas.addEventListener("click",function(event){
+    let e = event;
+    let startX = agent.x;
+    let startY= agent.y;
+
+    let endX = Math.floor((e.clientX - canvas.offsetLeft)/ block_length);
+    let endY= Math.floor((e.clientY - canvas.offsetTop)/block_length);
+
+    let path = findPath(startX,startY,endX,endY);
+
     main(path);
 });
 
@@ -239,15 +207,14 @@ function draw() {
 }
 
 function main(path) {
-
    for(let i = 0 ; i < path.length; i++){
        (function(time){
            window.setTimeout(function () {
-               agent.x = path[time][0] * block_length + block_length / 2;;
-               agent.y = path[time][1] * block_length + block_length / 2;;
+               agent.x = path[time][0];
+               agent.y = path[time][1];
                requestAnimationFrame(draw);
-               if(path[time][0] === file.x/block_length &&
-                   path[time][1] === file.y/block_length){
+               if(path[time][0] === file.x &&
+                   path[time][1] === file.y){
                    init_canvas();
                }
            }, 1000/20 * time);
